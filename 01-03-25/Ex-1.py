43b2f6a88fc70fcb71889be1319a209d3dfb5df1
ramudu7799 <jayaramgogula123@gmail.com>
2025-03-03 09:53:00 +0530

    Update Ex-1.py

--- 01-03-25/Ex-1.py
+++ 01-03-25/Ex-1.py
@@ -4,7 +4,7 @@ Print the first 10 natural numbers using for loop.
 try:
 	n = int(input('Enter number : ' ))
 	sum=0
-	for i in range(1,11):
+	for i in range(1,n+1):
 		sum+=i
 	print(F'{n} natural numbers sum : {sum}')
 		
